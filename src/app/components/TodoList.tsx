bd1e30b9bf203225ea7eccbf0f9323410aa8a772
toqvist <tobias-ahlqvist@outlook.com>
2023-09-05 21:00:57 +0200

    Setup drizzle

---
+++ src/app/components/TodoList.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { trpc } from '../_trpc/client'
+
+export default function TodoList() {
+
+  const getTodos = trpc.getTodos.useQuery()
+
+  return (
+    <div>{JSON.stringify(getTodos.data)}</div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import TodoList from "./components/TodoList"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world!
-    </main>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Todos</h1>
+      <TodoList>
+      </TodoList>    
+      </main>
   )
 }
